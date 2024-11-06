ee9af9ed2835d450000998ab6f49ed0a45f8da15
LujanSolo <lujansolo@proton.me>
2024-11-05 18:25:14 -0800

    added bonus fields for all skills and attributes

--- src/app/(sub pages)/character/page.js
+++ src/app/(sub pages)/character/page.js
@@ -1,12 +1,12 @@
 import Form from '../../../components/character/Form.jsx';
-import bg from '../../../../public/backgrounds/character-bg.jpg';
+import bg from '../../../../public/backgrounds/char-bg3.jpg';
 import Image from "next/image";
 import AttrSkills from '../../../components/character/AttrSkills.jsx';
 
 export default function Character() {
   return (
     <>
-      <Image priority sizes="100vw" src={bg} alt='background image' className="-z-50 fixed w-full h-full object-cover object-center inset-0 opacity-85" />
+      <Image priority sizes="100vw" src={bg} alt='background image' className="-z-50 fixed w-full h-full object-cover object-center inset-0" />
       <Form />
       <AttrSkills />
     </>
--- src/components/character/AttrSkills.jsx
+++ src/components/character/AttrSkills.jsx
@@ -5,89 +5,140 @@ const AttrBlock = [
   {
     name: "dexterity",
     value: 0,
+    bonus: 0,
     skills: [
-      { name: "blaster", value: 0, },
-      { name: "brawling parry", value: 0, },
-      { name: "dodge", value: 0, },
-      { name: "grenade", value: 0 },
-      { name: "heavy weapons", value: 0, },
-      { name: "melee parry", value: 0, },
-      { name: "melee", value: 0, }]
+      { name: "blaster", value: 0, bonus: 0, },
+      { name: "brawling parry", value: 0, bonus: 0, },
+      { name: "dodge", value: 0, bonus: 0, },
+      { name: "grenade", value: 0, bonus: 0, },
+      { name: "heavy weapons", value: 0, bonus: 0, },
+      { name: "melee parry", value: 0, bonus: 0, },
+      { name: "melee", value: 0, bonus: 0, }
+    ]
   },
   {
     name: "perception",
     value: 0,
+    bonus: 0,
     skills: [
-      { name: "bargain", value: 0 },
-      { name: "command", value: 0 },
-      { name: "con", value: 0 },
-      { name: "gambling", value: 0 },
-      { name: "stealth", value: 0 },
-      { name: "search", value: 0 }]
+      { name: "bargain", value: 0, bonus: 0, },
+      { name: "command", value: 0, bonus: 0, },
+      { name: "con", value: 0, bonus: 0, },
+      { name: "gambling", value: 0, bonus: 0, },
+      { name: "stealth", value: 0, bonus: 0, },
+      { name: "search", value: 0, bonus: 0, }
+    ]
   },
   {
     name: "strength",
     value: 0,
-    skills: [{ name: "brawling", value: 0 }, { name: "climbing/jumping", value: 0 }, { name: "lifting", value: 0 }, { name: "stamina", value: 0 }]
+    bonus: 0,
+    skills: [
+      { name: "brawling", value: 0, bonus: 0, }, 
+      { name: "climbing/jumping", value: 0, bonus: 0, }, 
+      { name: "lifting", value: 0, bonus: 0, }, 
+      { name: "stamina", value: 0, bonus: 0, }
+    ]
   },
   {
     name: "knowledge",
     value: 0,
-    skills: [{ name: "alien species", value: 0 }, { name: "cultures", value: 0 }, { name: "intimidation", value: 0 }, { name: "languages", value: 0 }, { name: "planetary systems", value: 0 }, { name: "streetwise", value: 0 }, { name: "survival", value: 0 }, { name: "value", value: 0 }]
+    bonus: 0,
+    skills: [
+      { name: "alien species", value: 0, bonus: 0, }, 
+      { name: "cultures", value: 0, bonus: 0, }, 
+      { name: "intimidation", value: 0, bonus: 0, }, 
+      { name: "languages", value: 0, bonus: 0, }, 
+      { name: "planetary systems", value: 0, bonus: 0, }, 
+      { name: "streetwise", value: 0, bonus: 0, }, 
+      { name: "survival", value: 0, bonus: 0, }, 
+      { name: "value", value: 0, bonus: 0, }
+    ]
   },
   {
     name: "mechanical",
     value: 0,
-    skills: [{ name: "astrogation", value: 0 }, { name: "beast riding", value: 0 }, { name: "communications", value: 0 }, { name: "repulsorlift operation", value: 0, }, { name: "space transports", value: 0 }, { name: "starship gunnery", value: 0 }, { name: "starship shields", value: 0 }, { name: "swoop operation", value: 0 }]
+    bonus: 0,
+    skills: [
+      { name: "astrogation", value: 0, bonus: 0, }, 
+      { name: "beast riding", value: 0, bonus: 0, }, 
+      { name: "communications", value: 0, bonus: 0, },
+      { name: "repulsorlift operation", value: 0, bonus: 0, }, 
+      { name: "space transports", value: 0, bonus: 0, }, 
+      { name: "starship gunnery", value: 0, bonus: 0, }, 
+      { name: "starship shields", value: 0, bonus: 0, }, 
+      { name: "swoop operation", value: 0, bonus: 0, }
+    ]
   },
   {
     name: "technical",
     value: 0,
-    skills: [{ name: "computer programming/repair", value: 0 }, { name: "demolitions", value: 0 }, { name: "droid programming/repair", value: 0 }, { name: "first aid", value: 0 }, { name: "security", value: 0 }, { name: "space transports repair", value: 0 }, { name: "starship weapon repair", value: 0 }]
+    bonus: 0,
+    skills: [
+      { name: "computer programming/repair", value: 0, bonus: 0, }, 
+      { name: "demolitions", value: 0, bonus: 0, }, 
+      { name: "droid programming/repair", value: 0, bonus: 0, }, 
+      { name: "first aid", value: 0, bonus: 0, }, 
+      { name: "security", value: 0, bonus: 0, }, 
+      { name: "space transports repair", value: 0, bonus: 0, }, 
+      { name: "starship weapon repair", value: 0, bonus: 0, }
+    ]
   }
 ]
 
 const AttrSkills = () => {
+
   let [attrPoints, setAttrPoints] = useState(18);
   const [attributes, setAttributes] = useState(AttrBlock);
 
-  const changeValue = (index, action) => {
+  const updateAttributeValue = (index, action) => {
     let newAttributes = [...attributes];
     if (action === 'add') {
       if (attrPoints > 0 && newAttributes[index].value < 6) {
-        newAttributes[index].value += 1;
-        setAttrPoints(attrPoints - 1);
+        newAttributes[index].value++;
+        setAttrPoints(prevPoints => prevPoints - 1);  // Update attrPoints immutably
         setAttributes(newAttributes);
+        //todo: set skill points based off of attribute value
+        //todo: update skill points from skill point pool
       }
     } else if (action === 'minus') {
-      if (newAttributes[index].value > 0) {
-        newAttributes[index].value -= 1;
-        setAttrPoints(attrPoints + 1);
+      if (newAttributes[index].value > 0 && attrPoints < 18) {
+        newAttributes[index].value--;
+        setAttrPoints(prevPoints => prevPoints + 1);  // Update attrPoints immutably
         setAttributes(newAttributes);
       }
-    }
+    };
   };
 
 
-
-
   return (
-    <div>
+    <div className="w-full flex flex-col items-center justify-center">
       <h1 className="text-accent">Attributes and Skills</h1>
       <div className="flex flex-wrap">
         {AttrBlock.map((attr, index) => (
           <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 custom-bg-alt p-4 m-2">
-            <h2 className="text-xl text-accent">
+            <h2 className="text-xl text-accent text-center capitalize">
               {attr.name}:
-              <span id="attr" className="text-sm text-foreground">
-                {attr.value}
+              <span id="attr" className="text-sm text-foreground px-1">
+                {attr.value}D
+              </span>
+              <span id="bonus" className="text-sm text-foreground ">
+                + {attr.bonus}
               </span>
             </h2>
-            <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => changeValue(index, 'add')}>+</button>
-            <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => changeValue(index, 'minus')}>-</button>
+            <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => updateAttributeValue(index, 'add')}>+</button>
+            <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => updateAttributeValue(index, 'minus')}>-</button>
             <ul>
               {attr.skills.map((skill, index) => (
-                <li key={index} className="text-sm text-accent">{skill.name}: <span className="text-sm text-foreground">{skill.value}</span></li>
+                <li key={index} className="text-sm text-accent capitalize">{skill.name}: <span className="text-sm text-foreground">{skill.value}</span>
+                  <span>
+                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, 'add')}>+</button>
+                  </span>
+                  <span>
+                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, 'minus')}>-</button>
+                  </span>
+                </li>
+
               ))}
             </ul>
           </div>
