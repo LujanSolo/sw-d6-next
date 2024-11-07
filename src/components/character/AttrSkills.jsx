bc32f8aeefdab2432c42985c966ceb1aeb249dfd
LujanSolo <lujansolo@proton.me>
2024-11-06 18:34:10 -0800

    reformulated AttrSkills function in updateSkillValue method to implement +2 skill point limit after basing skill points on parent attribute value

--- src/components/character/AttrSkills.jsx
+++ src/components/character/AttrSkills.jsx
@@ -34,9 +34,9 @@ const AttrBlock = [
     value: 0,
     bonus: 0,
     skills: [
-      { name: "brawling", value: 0, bonus: 0, }, 
-      { name: "climbing/jumping", value: 0, bonus: 0, }, 
-      { name: "lifting", value: 0, bonus: 0, }, 
+      { name: "brawling", value: 0, bonus: 0, },
+      { name: "climbing/jumping", value: 0, bonus: 0, },
+      { name: "lifting", value: 0, bonus: 0, },
       { name: "stamina", value: 0, bonus: 0, }
     ]
   },
@@ -45,13 +45,13 @@ const AttrBlock = [
     value: 0,
     bonus: 0,
     skills: [
-      { name: "alien species", value: 0, bonus: 0, }, 
-      { name: "cultures", value: 0, bonus: 0, }, 
-      { name: "intimidation", value: 0, bonus: 0, }, 
-      { name: "languages", value: 0, bonus: 0, }, 
-      { name: "planetary systems", value: 0, bonus: 0, }, 
-      { name: "streetwise", value: 0, bonus: 0, }, 
-      { name: "survival", value: 0, bonus: 0, }, 
+      { name: "alien species", value: 0, bonus: 0, },
+      { name: "cultures", value: 0, bonus: 0, },
+      { name: "intimidation", value: 0, bonus: 0, },
+      { name: "languages", value: 0, bonus: 0, },
+      { name: "planetary systems", value: 0, bonus: 0, },
+      { name: "streetwise", value: 0, bonus: 0, },
+      { name: "survival", value: 0, bonus: 0, },
       { name: "value", value: 0, bonus: 0, }
     ]
   },
@@ -60,13 +60,13 @@ const AttrBlock = [
     value: 0,
     bonus: 0,
     skills: [
-      { name: "astrogation", value: 0, bonus: 0, }, 
-      { name: "beast riding", value: 0, bonus: 0, }, 
+      { name: "astrogation", value: 0, bonus: 0, },
+      { name: "beast riding", value: 0, bonus: 0, },
       { name: "communications", value: 0, bonus: 0, },
-      { name: "repulsorlift operation", value: 0, bonus: 0, }, 
-      { name: "space transports", value: 0, bonus: 0, }, 
-      { name: "starship gunnery", value: 0, bonus: 0, }, 
-      { name: "starship shields", value: 0, bonus: 0, }, 
+      { name: "repulsorlift operation", value: 0, bonus: 0, },
+      { name: "space transports", value: 0, bonus: 0, },
+      { name: "starship gunnery", value: 0, bonus: 0, },
+      { name: "starship shields", value: 0, bonus: 0, },
       { name: "swoop operation", value: 0, bonus: 0, }
     ]
   },
@@ -75,12 +75,12 @@ const AttrBlock = [
     value: 0,
     bonus: 0,
     skills: [
-      { name: "computer programming/repair", value: 0, bonus: 0, }, 
-      { name: "demolitions", value: 0, bonus: 0, }, 
-      { name: "droid programming/repair", value: 0, bonus: 0, }, 
-      { name: "first aid", value: 0, bonus: 0, }, 
-      { name: "security", value: 0, bonus: 0, }, 
-      { name: "space transports repair", value: 0, bonus: 0, }, 
+      { name: "computer programming/repair", value: 0, bonus: 0, },
+      { name: "demolitions", value: 0, bonus: 0, },
+      { name: "droid programming/repair", value: 0, bonus: 0, },
+      { name: "first aid", value: 0, bonus: 0, },
+      { name: "security", value: 0, bonus: 0, },
+      { name: "space transports repair", value: 0, bonus: 0, },
       { name: "starship weapon repair", value: 0, bonus: 0, }
     ]
   }
@@ -91,6 +91,8 @@ const AttrSkills = () => {
   let [attrPoints, setAttrPoints] = useState(18);
   const [attributes, setAttributes] = useState(AttrBlock);
 
+  let [skillPoints, setSkillPoints] = useState(7);
+
   const updateAttributeValue = (index, action) => {
     let newAttributes = [...attributes];
     if (action === 'attr-add') {
@@ -98,20 +100,40 @@ const AttrSkills = () => {
         newAttributes[index].value++;
         setAttrPoints(prevPoints => prevPoints - 1);  // Update attrPoints immutably
         setAttributes(newAttributes);
-        //todo: set skill points based off of attribute value
         newAttributes[index].skills.forEach(skill => {
           skill.value = newAttributes[index].value;
         })
-        //todo: update skill points from skill point pool
       }
     } else if (action === 'attr-minus') {
       if (newAttributes[index].value > 0 && attrPoints < 18) {
         newAttributes[index].value--;
-        setAttrPoints(prevPoints => prevPoints + 1);  // Update attrPoints immutably
+        setAttrPoints(prevPoints => prevPoints + 1);
         setAttributes(newAttributes);
         newAttributes[index].skills.forEach(skill => {
           skill.value = newAttributes[index].value;
         })
+      }
+    };
+  };
+
+  const updateSkillValue = (attrIndex, skillIndex, action) => {
+    let newAttributes = [...attributes];
+    if (action === 'skill-add') {
+      if (
+        skillPoints > 0 &&
+        newAttributes[attrIndex].skills[skillIndex].value < newAttributes[attrIndex].value + 2
+      ) {
+        newAttributes[attrIndex].skills[skillIndex].value++;
+        setSkillPoints(prevPoints => prevPoints - 1);  // Update attrPoints immutably
+        setAttributes(newAttributes);
+      }
+    } else if (action === 'skill-minus') {
+      if (
+        newAttributes[attrIndex].skills[skillIndex].value > 0 && skillPoints < 7
+      ) {
+        newAttributes[attrIndex].skills[skillIndex].value--;
+        setSkillPoints(prevPoints => prevPoints + 1);
+        setAttributes(newAttributes);
       }
     };
   };
@@ -135,13 +157,13 @@ const AttrSkills = () => {
             <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => updateAttributeValue(index, 'attr-add')}>+</button>
             <button className="bg-accent text-foreground p-1 m-1 rounded" onClick={() => updateAttributeValue(index, 'attr-minus')}>-</button>
             <ul>
-              {attr.skills.map((skill, index) => (
-                <li key={index} className="text-sm text-accent capitalize">{skill.name}: <span className="text-sm text-foreground">{skill.value}</span>
+              {attr.skills.map((skill, skillIndex) => (
+                <li key={skillIndex} className="text-sm text-accent capitalize">{skill.name}: <span className="text-sm text-foreground">{skill["value"]}</span>
                   <span>
-                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, 'skill-add')}>+</button>
+                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, skillIndex, 'skill-add')}>+</button>
                   </span>
                   <span>
-                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, 'skill-minus')}>-</button>
+                    <button className="p-1 m-1 rounded" onClick={() => updateSkillValue(index, skillIndex, 'skill-minus')}>-</button>
                   </span>
                 </li>
 
